404eecd7dd9516a2f3d279462efe00df7483f616
Issam <Issam.aboulfadl05@gmail.com>
2023-01-22 21:36:10 +0100

    finish greatest calories puzzle part01

--- day1/part-1.js
+++ day1/part-1.js
@@ -5,19 +5,37 @@ const paresLine = async () => {
 	return data.toString().split("\n");
 };
 
-const solve = async () => {
+const getTotalCalories = async () => {
 	const input = await paresLine();
-	let totals = [];
+	let totalCalories = [];
 	let currentSum = 0;
 	input.forEach((item) => {
 		if (item === "\r") {
-			totals.push(currentSum);
+			totalCalories.push(currentSum);
 			currentSum = 0;
 		} else {
 			currentSum += Number(item);
 		}
 	});
-	totals.push(currentSum);
+	totalCalories.push(currentSum);
+	return totalCalories;
+};
+
+const solvePartOne = async () => {
+	const totals = await getTotalCalories();
 	return Math.max(...totals);
 };
-solve().then(console.log);
+solvePartOne().then(console.log);
+
+/* -------------------------------- solution part01 -------------------------------- */
+// puzzle answer is  74198
+
+async function solvePartTwo() {
+	const totals = await getTotalCalories();
+	totals.sort((a, b) => b - a);
+	return totals[0] + totals[1] + totals[2];
+}
+solvePartTwo().then(console.log);
+
+/* -------------------------------- solution part02 -------------------------------- */
+// puzzle answer is ❓ 209914
